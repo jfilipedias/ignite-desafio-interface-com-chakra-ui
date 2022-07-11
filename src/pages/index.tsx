32b0fa64fb07ba98de3dae592c2b7a3b287e7602
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-07-11 10:04:46 -0300

    Adds travel types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,52 @@
-import { Flex, Heading, HStack, Image, Stack, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
 import type { NextPage } from 'next';
+
+interface TravelTypes {
+  id: number;
+  label: string;
+  image: string;
+}
+
+const travelTypes: TravelTypes[] = [
+  {
+    id: 1,
+    label: 'vida noturna',
+    image: 'icons/cocktail.svg',
+  },
+  {
+    id: 2,
+    label: 'praia',
+    image: 'icons/surf.svg',
+  },
+  {
+    id: 3,
+    label: 'moderno',
+    image: 'icons/building.svg',
+  },
+  {
+    id: 4,
+    label: 'clássico',
+    image: 'icons/museum.svg',
+  },
+  {
+    id: 5,
+    label: 'e mais...',
+    image: 'icons/earth.svg',
+  },
+];
 
 const Home: NextPage = () => {
   return (
-    <Flex direction="column">
+    <Flex direction="column" alignItems="center" paddingBottom="40px">
       <Flex
         position="relative"
         width="100vw"
@@ -19,7 +62,7 @@ const Home: NextPage = () => {
           alignItems="flex-start"
         >
           <Stack maxWidth="530px" spacing="20px" marginTop="16px">
-            <Heading color="white" fontWeight="medium">
+            <Heading as="h2" color="white" fontWeight="medium">
               5 Continentes, infinitas possibilidades.
             </Heading>
             <Text color="gray.100" fontSize="20px">
@@ -30,7 +73,30 @@ const Home: NextPage = () => {
           <Image src="airplane.svg" />
         </HStack>
       </Flex>
-      <div>travel</div>
+
+      <Flex justifyContent="space-between" width="1200px" marginTop="120px">
+        {travelTypes.map(travelType => (
+          <Stack key={travelType.id} alignItems="center" width="160px">
+            <Image src={travelType.image} width="85px" height="85px" />
+            <Text
+              textAlign="center"
+              fontSize="24px"
+              fontWeight="semibold"
+              marginTop="24px"
+            >
+              {travelType.label}
+            </Text>
+          </Stack>
+        ))}
+      </Flex>
+
+      <Box background="gray.600" width="90px" height="2px" marginY="80px" />
+
+      <Heading as="h2" textAlign="center" fontSize="36px" fontWeight="medium">
+        Vamos nessa? <br />
+        Então escolha seu continente
+      </Heading>
+
       <div>continents</div>
     </Flex>
   );
