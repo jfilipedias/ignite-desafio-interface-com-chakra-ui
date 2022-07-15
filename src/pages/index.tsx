e80ff2abdc729fc2581454ab1899ba1645e1d171
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-07-15 17:14:30 -0300

    Starts to implement the swiper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import type { NextPage } from 'next';
+
+import { Keyboard, Pagination, Navigation } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import {
   Box,
-  Divider,
   Flex,
   Heading,
   HStack,
@@ -8,7 +13,10 @@ import {
   Stack,
   Text,
 } from '@chakra-ui/react';
-import type { NextPage } from 'next';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 interface TravelTypes {
   id: number;
@@ -44,26 +52,67 @@ const travelTypes: TravelTypes[] = [
   },
 ];
 
+const continents = [
+  {
+    id: 1,
+    name: 'Europa',
+    label: 'O continente mais antigo',
+    image:
+      'https://images.unsplash.com/photo-1473951574080-01fe45ec8643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=904&q=80',
+  },
+  {
+    id: 2,
+    name: 'América do Norte',
+    label: 'O continente mais antigo',
+    image:
+      'https://images.unsplash.com/photo-1483653364400-eedcfb9f1f88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  },
+  {
+    id: 3,
+    name: 'América do Sul',
+    label: 'O continente mais antigo',
+    image:
+      'https://images.unsplash.com/photo-1568805746970-0bbae56ab18b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+  },
+  {
+    id: 4,
+    name: 'África',
+    label: 'O continente mais antigo',
+    image:
+      'https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80',
+  },
+  {
+    id: 5,
+    name: 'Ásia',
+    label: 'O continente mais antigo',
+    image:
+      'https://images.unsplash.com/photo-1528164344705-47542687000d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=892&q=80',
+  },
+  {
+    id: 6,
+    name: 'Oceania',
+    label: 'O continente mais antigo',
+    image:
+      'https://images.unsplash.com/photo-1523482580672-f109ba8cb9be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1530&q=80',
+  },
+];
+
 const Home: NextPage = () => {
   return (
     <Flex direction="column" alignItems="center" paddingBottom="40px">
       <Flex
-        position="relative"
         width="100vw"
         height="335px"
+        backgroundImage="url(background.png)"
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        backgroundPosition="center"
         justifyContent="center"
       >
-        <Image src="background.png" objectFit="cover" />
-
-        <HStack
-          position="absolute"
-          top="80px"
-          spacing="100px"
-          alignItems="flex-start"
-        >
+        <HStack marginTop="80px" spacing="100px" alignItems="flex-start">
           <Stack maxWidth="530px" spacing="20px" marginTop="16px">
             <Heading as="h2" color="white" fontWeight="medium">
-              5 Continentes, infinitas possibilidades.
+              6 Continentes, infinitas possibilidades.
             </Heading>
             <Text color="gray.100" fontSize="20px">
               Chegou a hora de tirar do papel a viagem que você sempre sonhou.
@@ -97,7 +146,40 @@ const Home: NextPage = () => {
         Então escolha seu continente
       </Heading>
 
-      <div>continents</div>
+      <Box width="100%" maxWidth="1240px" maxHeight="450px" marginTop="52px">
+        <Swiper
+          slidesPerView={1}
+          keyboard={{
+            enabled: true,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation
+          modules={[Keyboard, Pagination, Navigation]}
+        >
+          {continents.map(continent => (
+            <SwiperSlide key={continent.id}>
+              <Flex
+                width="100%"
+                height="100%"
+                alignItems="center"
+                justifyContent="center"
+                backgroundImage={continent.image}
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                backgroundPosition="center"
+                textAlign="center"
+              >
+                <Stack>
+                  <Heading>{continent.name}</Heading>
+                  <Text>{continent.label}</Text>
+                </Stack>
+              </Flex>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
     </Flex>
   );
 };
