9143e550918c2f0e04dce0c3562ccae02da9927e
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2022-07-20 09:37:49 -0300

    Adds the back button to Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import { Box, Flex, useBreakpointValue } from '@chakra-ui/react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Header: React.FC = () => {
-  const imageWidth = useBreakpointValue({ base: 81, md: 184.06 });
-  const imageHeight = useBreakpointValue({ base: 20, md: 45.92 });
+  const logoWidth = useBreakpointValue({ base: 81, md: 184.06 });
+  const logoHeight = useBreakpointValue({ base: 20, md: 45.92 });
+
+  const route = useRouter();
+
+  const isHome = route.pathname === '/';
 
   return (
     <Flex
@@ -14,16 +19,40 @@ const Header: React.FC = () => {
       background="white"
       height={{ base: '50px', md: '100px' }}
     >
-      <Link href="/">
-        <a>
-          <Image
-            src="/logo.svg"
-            alt="logo"
-            width={imageWidth}
-            height={imageHeight}
-          />
-        </a>
-      </Link>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        position="relative"
+        width="100%"
+        maxWidth="1160px"
+        marginX="20px"
+      >
+        {!isHome && (
+          <Box position="absolute" left={{ base: '16px', md: '0px' }}>
+            <Link href="/" passHref>
+              <a>
+                <Image
+                  src="/icons/leftArrow.svg"
+                  alt="back to home"
+                  width="8px"
+                  height="16px"
+                />
+              </a>
+            </Link>
+          </Box>
+        )}
+
+        <Link href="/" passHref>
+          <a>
+            <Image
+              src="/logo.svg"
+              alt="logo"
+              width={logoWidth}
+              height={logoHeight}
+            />
+          </a>
+        </Link>
+      </Flex>
     </Flex>
   );
 };
